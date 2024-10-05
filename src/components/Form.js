91880bb0ef5b798e72af40f5c9d778ec625662bc
Pieplane <smolkin_97@inbox.ru>
2024-10-05 23:15:48 +0700

    Реализован компонент обработки полей для ввода

---
+++ src/components/Form.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+import '../styles/components/form.css';
+
+const Form = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [inputValue1, setInputValue1] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    const handleInputChange1 = (e) => {
+        setInputValue1(e.target.value);
+    };
+
+    const clearInput = () => {
+        setInputValue('');
+        setInputValue1('');
+    };
+
+    return (
+        <div>
+        <div className="from">
+            <div className="pickUpLocation">
+                PICKUP LOCATION
+            </div>
+            <div className="input1" img src="/images/input.png" alt="input" />
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Введите значение"
+                className="inputPickUp"
+                />
+        </div>
+
+        <div className="to">
+            <div className="dropLocation">
+                DROP LOCATION
+            </div>
+            <div className="input1" img src="/images/input.png" alt="input" />
+            <input
+                type="text"
+                value={inputValue1}
+                onChange={handleInputChange1}
+                placeholder="Введите значение"
+                className="inputDrop"
+                />
+        </div>
+        <div className="btn_order" style={{ cursor: 'pointer' }}>
+            <div className="btn_request"> 
+                Order
+            </div>
+            <div className="btn_fill" >
+            </div>
+        </div>
+        <div className="clearAll">
+            
+        </div>
+        <div className="vectorClear" img src="/images/Vector1.png" alt="clear" />
+        <div className="clearAllText" onClick={clearInput} style={{ cursor: 'pointer' }}>
+            Clear All
+        </div>
+        </div>
+    );
+};
+
+export default Form;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import '../styles/components/main.css';
 import Tabs from './tabs';
+import Form from './Form';
 
 const GetStarted = () =>(
     <div className='main'>
@@ -11,8 +12,18 @@ const GetStarted = () =>(
                 <div className='i'>
                     <img src="images/i.png" alt="i" />
                 </div>
+                <div className='pin'>
+                <img src="images/Pin.png" alt="Pin" />
+                    </div>
+                    <div className='pin1'>
+                <img src="images/Pin1.png" alt="Pin1" />
+                    </div>
+                    <div className='vector'>
+                <img src="images/Vector.png" alt="Vector" />
+                    </div>
             <div className='pad'></div>
             <Tabs />
+            <Form />
             </div>
             
         </div>
